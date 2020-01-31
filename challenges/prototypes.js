1c5ceb6db002ee9212006efbacb494937918b006
Eddie Madrigal <edmadrigal@yahoo.com>
2020-01-31 12:50:02 -0800

    refactoring made

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -1,12 +1,10 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-class CuboidMaker {
-  constructor(length, width, height) {
-    this.length = length;
-    this.width = width;
-    this.height = height;
-  }
+function CuboidMaker (length, width, height) {
+  this.length = length;
+  this.width = width;
+  this.height = height;
 }
 
 /* == Step 2: Volume Method ==
